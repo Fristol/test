ef526f7976d122a3b72fbf19816e1e72e94291c6
Lev <klimof.lev@gmail.com>
2022-04-28 02:31:01 +0500

    edit

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 async function get() {
-    let url = await fetch('http://localhost/redis/api/redis');
+    let url = await fetch(window.location.href + 'api/redis');
     let res = await url.json();
     document.querySelector('.list').innerHTML = '';
     if (res.status === true) {
         for (const [key, value] of Object.entries(res.data)) {
             document.querySelector('.list').innerHTML +=
-                `<li>${key}: ${value} <a href="#" class="remove" onclick="remove('${key}')">delete</a></li>`;
+                `<li>${key}: ${value} <a href="" class="remove" onclick="remove('${key}')">delete</a></li>`;
         }
     }
 }
 
 async function remove(key) {
-    let url = await fetch(`http://localhost/redis/api/redis/${key}`, {
+    let url = await fetch(window.location.href + `api/redis/${key}`, {
         method: "DELETE"
     });
     let res = await url.json();
